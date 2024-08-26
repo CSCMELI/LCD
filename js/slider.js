c4c1d3797ef56dd481df71253f5221704d5da24b
CSC91 <angelalbertocordovaenriquez@gmail.com>
2024-08-26 08:30:43 -0600

    a

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,11 @@
-(function(){
-    
-    const sliders = [...document.querySelectorAll('.testimony__body')];
-    const buttonNext = document.querySelector('#next');
-    const buttonBefore = document.querySelector('#before');
-    let value;   
+let slideIndex = 0;
+showSlides();
 
-    buttonNext.addEventListener('click', ()=>{
-        changePosition(1);
-    });
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    buttonBefore.addEventListener('click', ()=>{
-        changePosition(-1);
-    });
-
-    const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
-        value+= add;
-
-
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
-        }
-
-        sliders[value-1].classList.add('testimony__body--show');
-
-    }
-
-})();
+function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    if (n >= slides.length) {slideIndex = 0}
+}
