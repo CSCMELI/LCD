8ed2bd1df0f3511136c442990d63f4abe37703c5
CSC91 <angelalbertocordovaenriquez@gmail.com>
2024-08-26 14:41:26 -0600

    a

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,30 @@
 let slideIndex = 0;
-showSlides();
+showSlides(slideIndex);
 
+// Función para avanzar o retroceder entre las diapositivas
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// Función para mostrar la diapositiva actual
 function showSlides(n) {
     let slides = document.querySelectorAll('.slide');
-    if (n >= slides.length) {slideIndex = 0}
+    
+    // Si el índice es mayor que la cantidad de diapositivas, vuelve a la primera
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    
+    // Si el índice es menor que 0, muestra la última diapositiva
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    // Oculta todas las diapositivas
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+    
+    // Muestra la diapositiva actual
+    slides[slideIndex].style.display = "block";
 }
